093697c3cea0bb1d81dce588ad3051980fe7a641
Ahmed Abdelaliem <ahmed.abdelaliem@gmail.com>
2020-05-04 22:39:22 +0100

    Add json response

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/app'));
-
-// Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/app/index.html'));
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ "hello": "world!" }));
 });
 
 // default Heroku PORT
