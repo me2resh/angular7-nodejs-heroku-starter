03b5eb4b7c4e1a44d3d513daaac895858e0c8417
Ahmed Abdelaliem <ahmed.abdelaliem@gmail.com>
2020-05-04 22:47:01 +0100

    Serve from angular

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static files....
+app.use(express.static(__dirname + '/dist/angular-deploy'));
+
+// Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ "hello": "world!" }));
+  res.sendFile(path.join(__dirname + '/dist/angular-deploy/index.html'));
 });
 
 // default Heroku PORT
